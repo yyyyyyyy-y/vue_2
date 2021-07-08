a9778f974d12e21341f88d9457040b65bfa0d925
yyyyyyyy-y <1974288772@qq.com>
2021-07-08 09:41:45 +0800

    安装插件

---
+++ src/views/login/index.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="login-container">
+  <div class="login-box"></div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped lang="scss">
+.login-container{
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(to bottom,orange,red);
+  .login-box{
+    width: 40%;
+    height: 30%;
+    background: darksalmon;
+  }
+}
+</style>
